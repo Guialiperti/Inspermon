6d5322526e9e09b5bebbf0471c11635a3a4d5aeb
Guiizika <gui.aliperti9@gmail.com>
2017-04-04 10:33:43 -0300

    Debug Função batalha melhorada sorte e acertos críticos

--- EP2 - 1 versao.py
+++ EP2 - 1 versao.py
@@ -1,22 +1,53 @@
 import random
 import json
+import time
+def acerto_critico(sorte, dano):
+	sorteado = random.randint(0,100)
+	critico = dano * 2
+	if sorteado in (0, sorte):
+		return critico
+	else:
+		return dano
+
+
 def batalha(jogador,adversario):
     vida_jogador = jogador["vida"]
     vida_adversario = adversario["vida"]
+    dano_jogador = jogador["poder"] - adversario["defesa"]
+    dano_adversario = adversario["poder"] - jogador["defesa"]
+    if dano_jogador < 0:
+        dano_jogador = 0
+    if dano_adversario < 0:
+    	dano_adversario = 0
+    print("""Combate inicia!
+    	{0} vs {1}""".format(jogador["nome"], adversario["nome"]))
+    time.sleep(2)
     while vida_jogador > 0 and vida_adversario > 0:
-        vida_adversario = vida_adversario - (jogador["poder"] - adversario["defesa"])
+        vida_adversario = vida_adversario - dano_jogador
+        if vida_adversario < 0:
+        	vida_adversario = 0
+        print("{0}: Ataca! {1} de dano".format(jogador["nome"], dano_jogador))
+        print("{0}: {1} restante de vida".format(adversario["nome"], vida_adversario))
+        time.sleep(2)
         if vida_adversario < 1:
             return "VITÓRIA!"                               
-        vida_jogador = vida_jogador - (adversario["poder"] - jogador["defesa"])
+        vida_jogador = vida_jogador - dano_adversario
+        if vida_jogador < 0:
+        	vida_jogador = 0
+        print("{0}: Ataca! {1} de dano".format(adversario["nome"], dano_adversario))
+        print("{0}: {1} restante de vida".format(jogador["nome"], vida_jogador))
+        time.sleep(2)
         if vida_jogador < 1:
-            return "DERROTA!"   
-        
-                                             
+            return "DERROTA!"
+
+
 data = open("inspermons.json").read()
 inspermons = json.loads(data)
+
+
 z = 0
-for imon in inspermons:
-    print(imon)
+for ipmon in inspermons:
+    print(ipmon)
     print("Número do inspermon: {0}".format(z + 1)) 
     z = z+1
 x_do_jogador = int(input("Digite o número do inspermon escolhido: ")) - 1
@@ -27,12 +58,12 @@ poder: {2}
 defesa: {3}'''.format(inspermon_jogador["nome"],inspermon_jogador["vida"],inspermon_jogador["poder"],inspermon_jogador["defesa"]))   
 while True:
     acao = input("Digite lutar ou dormir: ")
-    inspermon_adversario = random.choice(inspermons)
-    print('''O inspermon adversário é: {0}
-vida: {1}
-poder: {2}
-defesa: {3}'''.format(inspermon_adversario["nome"],inspermon_adversario["vida"],inspermon_adversario["poder"],inspermon_adversario["defesa"])) 
     if acao == "dormir":
         break
     elif acao == "lutar":
-        print(batalha(inspermon_jogador,inspermon_adversario))
+	    inspermon_adversario = random.choice(inspermons)
+	    print('''O inspermon adversário é: {0}
+	vida: {1}
+	poder: {2}
+	defesa: {3}'''.format(inspermon_adversario["nome"],inspermon_adversario["vida"],inspermon_adversario["poder"],inspermon_adversario["defesa"])) 
+	    print(batalha(inspermon_jogador,inspermon_adversario))
